41ce67c9b93170adc60c2c7168dbce5f845f3132
hernanCelyNivelics <hernan.cely@nivelics.co>
2023-05-24 14:15:59 -0500

    add GSON

--- src/main/java/com/nivelics/materiasms/entity/Materia.java
+++ src/main/java/com/nivelics/materiasms/entity/Materia.java
@@ -1,11 +1,11 @@
 package com.nivelics.materiasms.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 
 @Entity
 @Data
